314b24b3077595b468535edb3bc6e2dbe2ad0784
flysand7 <thebumboni@gmail.com>
2024-09-15 08:31:57 +1100

    test cases

--- test.ps1
+++ test.ps1
@@ -3,4 +3,10 @@
 mkdir -p tmp/bin -erroraction 'silentlycontinue'
 nasm test/asm/mov16.asm -o tmp/bin/mov16
 
+# Build the assembler
+./build
+
+# Run the test files
+./x86-disasm ./tmp/bin/mov16 -cpu:16
+
 
